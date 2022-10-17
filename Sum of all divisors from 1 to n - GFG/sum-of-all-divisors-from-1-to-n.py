2fb4453076a43858bc1619a00798493d2db5673d
Pranav Agrawal <60977233+pranavagrawal321@users.noreply.github.com>
2022-10-18 02:38:19 +0530

    Added solution - LeetHub

---
+++ Sum of all divisors from 1 to n - GFG/sum-of-all-divisors-from-1-to-n.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+
+
+class Solution:
+    def sumOfDivisors(self, n):
+    	#code here 
+    	s = 0
+        for i in range(1, n+1):
+            s += n//i * i
+        return s
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ans = ob.sumOfDivisors(N)
+        print(ans)
+# } Driver Code Ends
