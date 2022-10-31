416ab2c81e52b33ec93fceeee797b2e32feb2d79
Pranav Agrawal <60977233+pranavagrawal321@users.noreply.github.com>
2022-10-31 20:11:31 +0530

    Time: 369 ms (20.06%), Space: 17.1 MB (5.30%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,3 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        a = 0
-        for i in nums:
-            a = a ^ i
-        return a
+        return 2*sum(set(nums)) - sum(nums)
