0899a82dd5f54a12e8e63f278db1f7dc0545fad9
Pranav Agrawal <60977233+pranavagrawal321@users.noreply.github.com>
2022-11-10 01:25:55 +0530

    Added solution - LeetHub

---
+++ First element to occur k times - GFG/first-element-to-occur-k-times.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+from collections import defaultdict
+
+class Solution:
+    def firstElementKTime(self,  arr, n, k):
+        # code here
+        d = defaultdict(int)
+        if k == 1:
+            return arr[0]
+        for i in arr:
+            d[i] += 1
+            if d[i] == k:
+                return i
+        return -1
+    
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        sz = [int(x) for x in input().strip().split()]
+        n, k = sz[0], sz[1]
+        a = [int(x) for x in input().strip().split()]
+        ob = Solution()
+        print(ob.firstElementKTime(a, n, k))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
