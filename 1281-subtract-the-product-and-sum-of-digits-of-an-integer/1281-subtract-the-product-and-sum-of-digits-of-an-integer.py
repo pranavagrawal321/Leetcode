9d34c6dd86d641f997c0717eb4a3341ef94a0c48
Pranav Agrawal <60977233+pranavagrawal321@users.noreply.github.com>
2022-12-04 04:05:09 +0530

    Create 1281-subtract-the-product-and-sum-of-digits-of-an-integer.py

---
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
@@ -0,0 +1,7 @@
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        p, s = 1, 0
+        for i in str(n):
+            p *= int(i)
+            s += int(i)
+        return p - s
