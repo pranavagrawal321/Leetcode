ea968c69ef3a2a2b2fb46b994ce7178b5f270599
Pranav Agrawal <60977233+pranavagrawal321@users.noreply.github.com>
2022-12-14 14:12:42 +0530

    Create 0075-sort-colors.py

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        low = 0
+        mid = 0
+        high = len(nums) - 1
+        for i in range(len(nums)):
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[high], nums[mid] = nums[mid], nums[high]
+                high -= 1
