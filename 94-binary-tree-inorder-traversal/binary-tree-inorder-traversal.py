24bfd02958af4552aabda466c5e9d67f6d11f697
Pranav Agrawal <60977233+pranavagrawal321@users.noreply.github.com>
2023-08-18 11:52:18 +0530

    Time: 46 ms (42.72%) | Memory: 16.3 MB (38.26%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -4,20 +4,8 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-
 class Solution:
-    l = []
-    def f(self,root):
-        if(root==None):
-            return
-        
-        self.f(root.left)
-        Solution().l.append(root.val)
-        self.f(root.right)
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        Solution().l.clear()
-        if root==None:
-            return Solution().l
-        self.f(root)
-        return Solution().l
+        if root is None:
+            return []
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
